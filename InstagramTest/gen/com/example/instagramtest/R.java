f24d13b8b4cd7b3301aa930fdf3f4801adabdc46
sergey <sergey@Dell.(none)>
2013-06-16 20:05:03 +0300

    third

--- InstagramTest/gen/com/example/instagramtest/R.java
+++ InstagramTest/gen/com/example/instagramtest/R.java
@@ -17,12 +17,11 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
@@ -33,15 +32,14 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int list_item=0x7f030001;
-        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int error_mes=0x7f040002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int error_mes=0x7f050002;
     }
     public static final class style {
         /** 
